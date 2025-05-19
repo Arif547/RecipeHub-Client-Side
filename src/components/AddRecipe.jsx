d109631769d84149803de305176debeda4954791
Mahabubul Alam Arif <mahabubulalamarif@gmail.com>
2025-05-19 22:47:28 +0600

    first commit

---
+++ src/components/AddRecipe.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddRecipe = () => {
+    return (
+        <div>
+            <h2 className="text-2xl font-bold mb-4">Add New Recipe</h2>
+        </div>
+    );
+};
+
+export default AddRecipe;
